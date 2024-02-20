34adb37f2329639b11a905c2f1933c5084a28c19
smcatingub <smcatingub@student.apc.edu.ph>
2024-02-20 09:29:24 +0800

    added news insert code ci4

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -10,7 +10,9 @@ $routes->get('/', 'Home::index');
 use App\Controllers\News;
 use App\Controllers\Pages;
 
-$routes->get('news', [News::class, 'index']);           
+$routes->get('news', [News::class, 'index']);
+$routes->get('news/new', [News::class, 'new']);
+$routes->post('news', [News::class, 'create']);           
 $routes->get('news/(:segment)', [News::class, 'show']); 
 
 $routes->get('pages', [Pages::class, 'index']);
